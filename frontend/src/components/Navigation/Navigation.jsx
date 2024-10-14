038d1b4dba29960740ca627d883a1ac3057b49d4
AbramBuck <abram.buck@gmail.com>
2024-10-14 11:57:15 -0500

    Styles import updated

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from '../Navigation/ProfileButton';
-import 'rental_database/frontend/src/components/Navigation/Navigation.css';
+import 'frontend/src/components/Navigation/Navigation.css';
 
 function Navigation({ isLoaded }) {
     const sessionUser = useSelector(state => state.session.user);
