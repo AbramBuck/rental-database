abfd47b6af0ba554f6c3f47965cb0895e4171d52
AbramBuck <abram.buck@gmail.com>
2024-10-14 12:04:04 -0500

    styles

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,13 +1,14 @@
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from '../Navigation/ProfileButton';
-import 'frontend/src/components/Navigation/Navigation.css';
+import styles from './Navigation.css';
 
 function Navigation({ isLoaded }) {
     const sessionUser = useSelector(state => state.session.user);
   
     return (
-        <div className='topnav'>
+        <div className={styles.topnav}>
                 <div>
                 <NavLink to="/">Home</NavLink>
                 </div>
